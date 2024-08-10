c2979dc4946ada1f3ff82bf1aa269557b508d086
Oguzhan YAMAK <oguzhanyamak@outlook.com>
2024-08-10 20:53:40 +0300

    OrderCreated iletişiminin kurulması

---
+++ Shared/Messages/OrderItemMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Messages
+{
+    public class OrderItemMessage
+    {
+        public Guid ProductId { get; set; }
+        public uint Count { get; set; }
+        public long Price { get; set; }
+    }
+}
